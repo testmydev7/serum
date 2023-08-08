7c9e3782f13ba387cf7419d72370bd37a1ecd92f
dagialez <dagialez@gmail.com>
2023-08-08 18:50:20 +0300

    mobileServiceCT

---
+++ src/api/mobile-header/routes/mobile-header.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mobile-header router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::mobile-header.mobile-header');
---
+++ src/api/mobile-header/services/mobile-header.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mobile-header service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::mobile-header.mobile-header');
