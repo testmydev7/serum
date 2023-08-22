bd2ce78606bc8883ae255dfb3cc5edb003e4ba7c
dagialez <dagialez@gmail.com>
2023-08-22 11:46:29 +0300

    service-testimony

---
+++ src/api/service-testimony/controllers/service-testimony.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * service-testimony controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::service-testimony.service-testimony');
---
+++ src/api/service-testimony/routes/service-testimony.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * service-testimony router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::service-testimony.service-testimony');
